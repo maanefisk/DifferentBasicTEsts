c0822bbe17699b92e19a9aef95aff07370b92a70
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-27 15:34:30 +0200

    Initial commit

---
+++ DifferentBasicTEsts/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DifferentBasicTEsts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write your name, please: ");
+            var nameInput = Console.ReadLine();
+            Console.WriteLine("Hello: " + nameInput + "!");
+        }
+    }
+}
